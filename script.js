8e02e0eef7f99748cec95ffc00c9da2761e899c2
DESKTOP-2P9DAPD\Windows 10 <j.kiwinicki@wp.pl>
2021-10-15 12:59:20 +0200

    mobile layout finished

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 (() => {
-	const map = L.map('map').setView([0, 0], 1);
+	// create map and set initial position
+	const map = L.map('map').setView([0, 0], 5);
 
-	const tilesURL = `https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png`;
+	// set zoom control on bottom left corner
+	map.zoomControl.setPosition('bottomleft');
+
+	const tilesURL = 'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png';
 	const attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors';
 
+	// add attribution
 	L.tileLayer(tilesURL, { attribution }).addTo(map);
 })();
